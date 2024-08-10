f4b13ef44ca1dc1330778359280eaa790b346ae4
Kopal Pareek <pareekkopal41@gmail.com>
2024-08-10 14:33:49 +0530

    Adding changes

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include<stdio.h>
 
 int main() {
-    cout << "Hello, World!" << endl;
+    std::cout << "Hello, World of c!" << std::endl;
     return 0;
 }
--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,64 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { exec } = require('child_process');
-const fs = require('fs'); // Required for file operations
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { exec } = require("child_process");
+const fs = require("fs");
 
 const app = express();
-const port = 5000;
+const port = 5001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post("/compile", async (req, res) => {
+  const { code, language, input } = req.body;
 
+  if (language === "java") {
+    // Write Java code to Main.java
+    fs.writeFileSync("Main.java", code);
 
-app.post('/compile', (req, res) => {
-  const { code, language } = req.body;
+    // Generate a unique filename for input
+    const inputFileName = `input_${Date.now()}.txt`;
 
-  let command = '';
+    // Write input to the unique input file
+    fs.writeFileSync(inputFileName, input);
 
-  
+    // Compile and execute the Java program, passing the unique input file as stdin
+    exec(
+      `javac Main.java && java Main < ${inputFileName}`,
+      (error, stdout, stderr) => {
+        // Delete the input file after execution
+        fs.unlinkSync(inputFileName);
 
-//   if (language === 'python') {
-//     command = python -c `"${code}"`;
-//   }
-//    else if (language === 'javascript') {
-//     command = node -e `"${code}"`;
-//   } 
-//   else
-   if (language === 'java') {
-    fs.writeFileSync('Main.java', code);
-    command = 'javac Main.java && java Main';
-  } 
-  else if (language === 'cpp') {
-    fs.writeFileSync('main.cpp', code);
-    command = 'gcc main.cpp -o main && ./main';
+        if (error) {
+          return res.status(500).json({ error: error.message });
+        }
+        if (stderr) {
+          return res.status(500).json({ error: stderr });
+        }
+        return res.json({ output: stdout });
+      }
+    );
+  } else if (language === "cpp") {
+    fs.writeFileSync("main.cpp", code);
+    exec(`gcc main.cpp -o main && ./main`, (error, stdout, stderr) => {
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
+      if (stderr) {
+        return res.status(500).json({ error: stderr });
+      }
+      return res.json({ output: stdout });
+    });
   } else {
-    return res.status(400).send('Unsupported language');
+    return res.status(400).send("Unsupported language");
   }
-
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).json({ error: stderr });
-    }
-    res.json({ output: stdout });
-  });
 });
 
-
-app.get('/', (req,res)=>{
-   return res.send("hello ")
-})
+app.get("/", (req, res) => {
+  return res.send("Hello, World!");
+});
 
 app.listen(port, () => {
-  console.log(`server is running on ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
